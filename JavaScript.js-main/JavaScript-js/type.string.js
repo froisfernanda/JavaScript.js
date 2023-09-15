dd8a6a3a85835d33fc01c23315c86fcb533639a7
froisfernanda <127303960+froisfernanda@users.noreply.github.com>
2023-09-15 09:48:35 -0300

    Add files via upload

---
+++ JavaScript.js-main/JavaScript-js/type.string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = "Olá, Mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu Nome é ";
+const MeuNome = "Fernanda";
+console.log(citacao)
+
+// concatação (+)
+
+
+console.log(citacao)
+
+//template string OU template literal
